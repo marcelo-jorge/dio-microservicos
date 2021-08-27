4d7f3057f6bbfdf76bba7ccf023ba3c320d831b2
Marcelo Bomfim <celodc@yahoo.com.br>
2021-08-26 22:54:30 -0300

    projeto micro-servicos

---
+++ catalagoDeProduto/src/test/java/com/dio/catalagoDeProduto/CatalagoDeProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.catalagoDeProduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalagoDeProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
