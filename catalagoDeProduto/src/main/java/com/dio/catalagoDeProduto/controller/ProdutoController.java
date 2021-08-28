b5733189a35643f089be99fd494fcf55cc05ef14
Marcelo Bomfim <celodc@yahoo.com.br>
2021-08-28 18:38:44 -0300

    produtocontroller ajuste de codigo

--- catalagoDeProduto/src/main/java/com/dio/catalagoDeProduto/controller/ProdutoController.java
+++ catalagoDeProduto/src/main/java/com/dio/catalagoDeProduto/controller/ProdutoController.java
@@ -23,29 +23,28 @@ public class ProdutoController {
         return produtoRepository.save(produto);
     }
 
-    @PutMapping
-    public Produto update(@RequestBody Produto produto){
-        return produtoRepository.save(produto);
-    }
-
-    @RequestMapping(method = RequestMethod.GET)
-    public Iterable<Produto> list() {
-        return produtoRepository.findAll();
-    }
-
-    @RequestMapping(method = RequestMethod.GET,value = "/{id}")
-    public Optional<Produto> findById(@PathVariable("id") Integer id) {
-        return produtoRepository.findById(id);
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/nome/{nome}")
-    public Iterable<Produto> findByName(@PathVariable("nome") String nome) {
-        return produtoRepository.findByNome(nome);
-    }
-
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Integer id){
         produtoRepository.deleteById(id);
     }
 
+    @PutMapping
+    public Produto update(@RequestBody Produto produto){
+        return produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public Iterable<Produto> list() {
+        return produtoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Produto> findById(@PathVariable Integer id) {
+        return produtoRepository.findById(id);
+    }
+
+    @GetMapping("/nome/{nome}")
+    public Iterable<Produto> findByName(@PathVariable String nome) {
+        return produtoRepository.findByNome(nome);
+    }
 }
