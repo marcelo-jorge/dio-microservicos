6047b9997e7d79f0f5f77e6b011f0896857717ca
Marcelo Bomfim <celodc@yahoo.com.br>
2021-08-29 10:25:09 -0300

    servidor configuracao ok

--- carrinhodeCompras/src/main/java/com/dio/carrinhoDeCompras/repository/CarrinhoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dio.carrinhoDeCompras.repository;
-
-import com.dio.carrinhoDeCompras.model.Carrinho;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CarrinhoRepository extends CrudRepository<Carrinho, Integer> {
-}
