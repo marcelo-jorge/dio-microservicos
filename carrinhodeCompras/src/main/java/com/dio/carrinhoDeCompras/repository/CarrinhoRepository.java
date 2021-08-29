626ef4fbf324403e36f50bd90b18eec0664f6d75
Marcelo Bomfim <celodc@yahoo.com.br>
2021-08-29 05:03:04 -0300

    carrinhoDeCompras classe repository

---
+++ carrinhodeCompras/src/main/java/com/dio/carrinhoDeCompras/repository/CarrinhoRepository.java
@@ -0,0 +1,7 @@
+package com.dio.carrinhoDeCompras.repository;
+
+import com.dio.carrinhoDeCompras.model.Carrinho;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarrinhoRepository extends CrudRepository<Carrinho, Integer> {
+}
