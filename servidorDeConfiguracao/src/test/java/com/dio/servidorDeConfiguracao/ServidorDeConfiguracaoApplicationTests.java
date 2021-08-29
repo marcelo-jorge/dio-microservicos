16a095712eb38e77ae5337ea0f0c36c04d287368
Marcelo Bomfim <celodc@yahoo.com.br>
2021-08-29 03:49:55 -0300

    carrinhoDeCompras inicio do projeto

---
+++ servidorDeConfiguracao/src/test/java/com/dio/servidorDeConfiguracao/ServidorDeConfiguracaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.servidorDeConfiguracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorDeConfiguracaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
